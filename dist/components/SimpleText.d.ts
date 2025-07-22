44660fdb2ce5b00f0058caf24a60c35b06839112
GabrielRoveree <gabriel.rovere@matrixenergia.com>
2025-07-22 18:17:10 -0300

    Initialize Vite + React + TypeScript project with basic components and configuration files.

---
+++ dist/components/SimpleText.d.ts
@@ -0,0 +1,3 @@
+export declare const SimpleText: ({ text }: {
+    text: string;
+}) => import("react/jsx-runtime").JSX.Element;
---
+++ src/components/SimpleText.tsx
@@ -0,0 +1,3 @@
+export const SimpleText = ({ text }: { text: string }) => {
+  return <div className="text-sm text-gray-700 dark:text-gray-300">{text}</div>;
+};
